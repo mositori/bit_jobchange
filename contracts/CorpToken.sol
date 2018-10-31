ba6507d8b9a11cbb66319c396d0ba331df684944
Keisuke Shimotori <destrage1958@gmail.com>
2018-11-01 00:38:49 +0900

    add getToken and getDetailOfToken functions

--- contracts/CorpToken.sol
+++ contracts/CorpToken.sol
@@ -15,15 +15,29 @@ contract CorpToken is ERC721Full, ERC721Mintable{
 
     // functions
     // defining the token's property its name and unit.
-    constructor() public ERC721Full("CorpToken", "CORP")  {
-        administrator = msg.sender;
-    }
+    constructor() public ERC721Full("CorpToken", "CORP")  {}
 
-    // registering corporation's address to mapping "isCorp".
-
-    // minting new token. this function can only be executed by registered corporations.
-    function mint(uint16 _eventId, uint16 _skill, uint16 _personallity, string _date) public {
+    // minting new token and transfering the token.
+    function mintAndTransfer(address _to, uint16 _eventId, uint16 _skill, uint16 _personallity, string _date) public {
         uint256 id = corpToken.push(CorpToken(_eventId, _skill, _personallity, _date)) - 1;
         super._mint(msg.sender, id);
+        transferFrom(msg.sender, _to, id);
+    }
+    
+    // @return eventId, skill, personallity, date
+    function getDetailOfToken(uint _tokenId) public view returns(uint16 _eventId, uint16 _skill, uint16 _personallity, string _date) {
+        _eventId = corpToken[_tokenId].eventId;
+        _skill = corpToken[_tokenId].skill;
+        _personallity = corpToken[_tokenId].personallity;
+        _date = corpToken[_tokenId].date;
+    }
+    
+    //@return Array including tokenId _owner has.
+    function getToken(address _owner) public view returns(uint[]){
+        uint[] memory _temp = new uint[](balanceOf(_owner));
+        for(uint i = 0; i < balanceOf(_owner); i++){
+            _temp[i] = tokenOfOwnerByIndex(_owner, i);
+        }
+        return _temp;
     }
 }
