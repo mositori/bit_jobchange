bd5a3d02969033204b86e45f9e2572e7b357679e
Keisuke Shimotori <destrage1958@gmail.com>
2018-11-02 20:50:52 +0900

    add main.js

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -5,20 +5,29 @@ import "./openzeppelin-solidity/contracts/token/ERC721/ERC721Mintable.sol";
 contract Token is ERC721Full, ERC721Mintable{
     struct UserToken {
         uint16 skill;
-        uint16 personalitty;
-        string mintedBy;
+        uint16 personality;
     }
 
     UserToken[] private userToken;
 
-    constructor() public ERC721Full("UserToken", "UTKN")  {}
+    constructor() public ERC721Full("UserToken", "USER")  {}
 
-    function mint(uint16 _skill, uint16 _personalitty, string _mintedBy) public {
-        uint256 id = userToken.push(UserToken(_skill, _personalitty, _mintedBy)) - 1;
+    function mintAndTransfer_USER(address _to, uint16 _skill, uint16 _personality) public {
+        uint256 id = userToken.push(UserToken(_skill, _personality)) - 1;
         super._mint(msg.sender, id);
+        transferFrom(msg.sender, _to, id);
     }
 
-    function getUserToken(uint256 _tokenId) public view returns (address, uint16, uint16, string, address) {
-        return (ownerOf(_tokenId), userToken[_tokenId].skill, userToken[_tokenId].personalitty, userToken[_tokenId].mintedBy, getApproved(_tokenId));
+    function getDetailOfToken_USER(uint256 _tokenId) public view returns (uint16 _skill, uint16 _personality) {
+        _skill = userToken[_tokenId].skill;
+        _personality = userToken[_tokenId].personality;
+    }
+
+    function getToken_USER(address _owner) public view returns(uint[]){
+        uint[] memory _temp = new uint[](balanceOf(_owner));
+        for(uint i = 0; i < balanceOf(_owner); i++){
+            _temp[i] = tokenOfOwnerByIndex(_owner, i);
+        }
+        return _temp;
     }
 }
--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,22 @@
+// variables
 let web3js; //for MetaMask
 let abi;
 let address;
-let contract;
-let hostAddress = "";
+let tokenContract;
+let corpTokenContract;
+let userAccount = "";
 let guestAddress = "";
+
+var accountInterval = setInterval(function(){
+  if(web3js.eth.accounts[0] !== userAccount){
+    userAccount = web3js.eth.accounts[0];
+  }
+},100)
+
 
 init();
 
+// defining function
 function init(){
   if (typeof web3 !== 'undefined') {
     web3js = new Web3(web3.currentProvider);
@@ -33,23 +43,37 @@ function init(){
       console.error("cannot get accounts")
       return;
     }else{
-      hostAddress = accounts;
+      userAccount = accounts;
       console.log(accounts);
     }
   })
   )
   .then(function(){
-    contract = web3js.eth.contract(abi).at(address);
-    console.log("contract setting complete!");
+    corpTokenContract = web3js.eth.contract(abi).at(address);
+    console.log("CorpToken.sol setting complete!");
+  })
+  $.ajax({
+    type: "GET",
+    url: "../build/contracts/Token.json",
+    datatype: "json"
+  })
+  .then(function(json){
+    abi = json["abi"];
+    address = json["networks"]["5777"]["address"];
+  })
+  .then(function(){
+    tokenContract = web3js.eth.contract(abi).at(address);
+    console.log("Token.sol setting complete!")
   })
 }
 
-// functions
 
+// Functions based on CorpToken.sol
+// mintAndTransfer
 function mintAndTransfer(_to, _eventId, _skill, _personallity, _date){
-  contract.mintAndTransfer.sendTransaction(
+  corpTokenContract.mintAndTransfer.sendTransaction(
     _to, _eventId, _skill, _personallity, _date,
-    {from:hostAddress[0], gas:1000000},
+    {from:userAccount, gas:1000000},
     (err,res) => {
       if(!err){
         console.log("You minted new Token and transfer to "+ _to + "!");
@@ -61,7 +85,7 @@ function mintAndTransfer(_to, _eventId, _skill, _personallity, _date){
 
 //return array including tokenId whose owner has.
 function getToken(_owner){
-  contract.getToken.call(_owner, (err,res) => {
+  corpTokenContract.getToken.call(_owner, (err,res) => {
     if(!err){
       console.log(res);
     }else{
@@ -70,8 +94,9 @@ function getToken(_owner){
   })
 }
 
+//return the token's contents
 function getDetailOfToken(_tokenId){
-  contract.getDetailOfToken.call(_tokenId, (err,res) => {
+  corpTokenContract.getDetailOfToken.call(_tokenId, (err,res) => {
     if(!err){
       console.log(res);
     }else{
@@ -80,3 +105,56 @@ function getDetailOfToken(_tokenId){
   })
 }
 
+
+// Functions based on Token.sol
+
+function mintAndTransfer_USER(_to, _skill, _personality){
+  tokenContract.mintAndTransfer_USER.sendTransaction(
+    _to, _skill, _personality,
+    {from: userAccount, gas: 1000000},
+    (err,res) => {
+      if(!err){
+        console.log("You minted new UserToken and transfer it to "+ _to + "!");
+      }else{
+        console.log(err);
+      }
+    }
+  )
+}
+
+
+function getToken_USER(_owner){
+  tokenContract.getToken_USER(_owner, function(err,res){
+    if(!err){
+      //console.log(result + "is TokenId");
+      //return result[0];
+      console.log(res);
+    }else{
+      console.log(err);
+    }
+  })
+}
+
+function getDetailOfToken_USER(_tokenId){
+  tokenContract.getDetailOfToken_USER(_tokenId, function(err,result){
+    if(!err){
+      let resSkill = result[0]["c"][0];
+      let resPersonality = result[1]["c"][0];
+      console.log("skill is "+ resSkill + ", personallity is " + resPersonality);
+      return resSkill, resPersonality;
+    }else{
+      console.log(err);
+    }
+  })
+}
+
+function getTotalSkill(_owner){
+  let _tokenIds = getToken_USER(_owner);
+  let totalSkill = 0;
+  let totalPersonality = 0;
+
+  for(let i of _tokenIds){
+    totalSkill, totalPersonality += getDetailOfToken_USER(i);
+  }
+  console.log(totalSkill, totalPersonality);
+}
